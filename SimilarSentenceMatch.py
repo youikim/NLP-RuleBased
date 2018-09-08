233ce4a0bb0dd50a92478a9cb18b55f0c51c4008
Jared <kuil5230@gmail.com>
2018-09-08 12:16:21 +0800

    .

--- SimilarSentenceMatch.py
+++ SimilarSentenceMatch.py
@@ -5,83 +5,69 @@ import csv
 from argparse import ArgumentParser
 
 
-
 class Similar():
-    
-    def __init__(self, as_lib = False):
-        
-        #相似度指標
-        self.Similarity_threshold  = 0.1
-        
-        #參數設定
+
+    def __init__(self, as_lib=False):
+
+        # 相似度指標
+        self.Similarity_threshold = 0.1
+
+        # 參數設定
         parser = ArgumentParser()
-        
-        parser.add_argument("-th", default = "0.1" , type = float , help = "threshold 0.1-1.0 ")
-        parser.add_argument("-s",  type = str , help = "save_path ")
-        parser.add_argument("-name" , type = str , help = "save_name ")
-        parser.add_argument("-t", default = "C:/Users/MB207-1/Desktop/8_29/8_27/Yuntech/3a_Rulebased/data/GenerateSentenceList(1).json" , type = str , help = "tranform json file")
-        parser.add_argument("-g", default = "C:/Users/MB207-1/Desktop/8_29/8_27/Yuntech/3a_Rulebased/data/ReferenceSentenceList.json" , type = str , help = "Good sentence json file ")
+
+        parser.add_argument("-th", default="0.1", type=float,
+                            help="threshold 0.1-1.0 ")
+        parser.add_argument("-s",  type=str, help="save_path ")
+        parser.add_argument("-name", type=str, help="save_name ")
+        parser.add_argument(
+            "-t", default="GenerateSentenceList(1).json", type=str, help="tranform json file")
+        parser.add_argument("-g", default="ReferenceSentenceList.json",
+                            type=str, help="Good sentence json file ")
         args = parser.parse_args()
-        
+
         if not as_lib:
             self.Comparison(args.th, args.s, args.name,  args.t, args.g)
 
-    
     def similarity(self, stringOne, stringTwo):
         d = distance.levenshtein(stringOne, stringTwo)
-        longest = max(len(stringOne), len(stringTwo));
-        return (longest - d) / longest;
-    
-    
-    def Comparison(self \
-                   , threshold \
-                   , save_path \
-                   , save_name \
-                   , GenerateSentenceListPath = "C:/Users/MB207-1/Desktop/8_29/8_27/Yuntech/3a_Rulebased/data/GenerateSentenceList(1).json" \
-                   , ReferenceSentenceListPath = "C:/Users/MB207-1/Desktop/8_29/8_27/Yuntech/3a_Rulebased/data/ReferenceSentenceList.json"
+        longest = max(len(stringOne), len(stringTwo))
+        return (longest - d) / longest
+
+    def Comparison(self, threshold, save_path, save_name, GenerateSentenceListPath="C:/Users/MB207-1/Desktop/8_29/8_27/Yuntech/3a_Rulebased/data/GenerateSentenceList(1).json", ReferenceSentenceListPath="C:/Users/MB207-1/Desktop/8_29/8_27/Yuntech/3a_Rulebased/data/ReferenceSentenceList.json"
                    ):
-        
-        
-         #替換後生成句的檔案
+
+         # 替換後生成句的檔案
         GS_path = GenerateSentenceListPath
 
-        #優良句檔案(作為匹配生成句相似度用)
+        # 優良句檔案(作為匹配生成句相似度用)
         RS_path = ReferenceSentenceListPath
-        
+
         save = []
-        
-        
-        with open(GS_path, 'r', encoding = 'utf-8') as GenerateSentenceData:
+
+        with open(GS_path, 'r', encoding='utf-8') as GenerateSentenceData:
             GenerateSentenceListPath = json.load(GenerateSentenceData)
- 
+
             GenerateSentenceData.close()
 
-        with open(RS_path, 'r', encoding = 'utf-8') as ReferenceSentenceData:
+        with open(RS_path, 'r', encoding='utf-8') as ReferenceSentenceData:
             ReferenceSentenceListPath = json.load(ReferenceSentenceData)
 
             ReferenceSentenceData.close()
-        
+
         for GenerateSentence in GenerateSentenceListPath:
             for ReferenceSentence in ReferenceSentenceListPath:
-                if(self.similarity(GenerateSentence,ReferenceSentence) > self.Similarity_threshold ):
-                    print('相似度=',round(self.similarity(GenerateSentence,ReferenceSentence),3),'優良句=',ReferenceSentence,'相似句=',GenerateSentence)
-                    save.append('相似度= ' + str(round(self.similarity(GenerateSentence,ReferenceSentence),3)) + \
-                                '優良句= ' + str(ReferenceSentence) + \
+                if(self.similarity(GenerateSentence, ReferenceSentence) > self.Similarity_threshold):
+                    print('相似度=', round(self.similarity(GenerateSentence, ReferenceSentence),
+                                        3), '優良句=', ReferenceSentence, '相似句=', GenerateSentence)
+                    save.append('相似度= ' + str(round(self.similarity(GenerateSentence, ReferenceSentence), 3)) +
+                                '優良句= ' + str(ReferenceSentence) +
                                 '相似句= ' + GenerateSentence)
-        
-        with open(save_path + save_name + '.csv', "w", encoding = "utf-8-sig") as file:
+
+        with open(save_path + save_name + '.csv', "w", encoding="utf-8-sig") as file:
             write = csv.writer(file)
             for s in save:
                 write.writerows(zip([s]))
-                
+
 
 if __name__ == '__main__':
-    S = Similar()                    
-                    
-      
-
-    
-
-
-
-
+    S = Similar()
